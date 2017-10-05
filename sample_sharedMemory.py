23261e5b455bca0b01a76ab9ef474f9848227614
Yagishita <yagishiaakirhio@okadanet.org>
2017-10-05 20:39:30 +0900

    first commit

---
+++ sample_sharedMemory.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from multiprocessing import Value, Array, Process
+
+def f5(n, a):
+    n.value = 3.1415926
+    for i in range(len(a)):
+        a[i] *= -1
+
+if __name__ == "__main__":
+    # 共有メモリ（Value）を作成します.
+    num = Value('d', 0.0)
+    # 共有メモリ（Array）を作成します.
+    arr = Array('i', range(10))
+    # サブプロセスを作り、実行します.
+    p = Process(target=f5, args=(num, arr))
+#    p = Process(target=f5, args=(arr))
+    p.start()
+    p.join()
+    # 共有メモリ（Value）から値を取り出します
+    print(num.value)
+    # 共有メモリ（Array）から値を取り出します
+    print(arr[:])
